90eb95186d2a1e9f183bd980e180a492f846944c
Sina Lewis <sile6618@colorado.edu>
2022-05-10 15:28:35 -0600

    Set up initial parameters
    Initialized particle positions and velocities

--- src/ActiveBrownianParticles.jl
+++ src/ActiveBrownianParticles.jl
@@ -1,5 +1,30 @@
-module ActiveBrownianParticles
+using Random
 
-# Write your package code here.
+module ActiveBrownianParticles
+# Define simulation params
+N = 1000    # number of particles
+boxSize = 25    # size of box
+T = 3.0     # temperature
+
+# Define random number generator
+rng = MersenneTwister()
+
+# Define starting points
+# Randomly place in box
+posDist = rand(rng, Float64, (N, 2)) .* boxSize
+
+# Define starting velocity
+# Draw from normal distribution
+velDist = randn(rng, Float64, (N, 2))
+
+# determine scaling factor for temperature
+KE = sum(velDist[:, 1] .^ 2 + velDist[:, 2] .^ 2)
+λ = sqrt(N * T / KE)
+# normalize velocity distriution to the temperature
+velDist = λ .* velDist
+
+######################
+
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
-using ActiveBrownianParticles
+using ActiveBrownianParticles, Random
+using Plots
 using Test
+
+rng = Random.MersenneTwister()
+
+posDist=rand(rng,Float64,(1000,2))
+sum(posDist[:,1].^2)
+
+
+plot(posDist[:,1],posDist[:,2],seriestype = :scatter)
+
+vel = ActiveBrownianParticles()
 
 @testset "ActiveBrownianParticles.jl" begin
     # Write your tests here.
